09fceaba01068773cb8ace2838c6d77bc5a0de0c
Armored147 <sarozos@Rozo.localdomain>
2026-04-18 08:40:02 -0500

    rutas

--- app/app.py
+++ app/app.py
@@ -4,32 +4,34 @@ from .calculadora import sumar, restar, multiplicar, dividir
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("index.html", resultado=None)
 
 
-
 @app.route("/calcular", methods=["POST"])
+def calcular():
     try:
-            num1 = float(request.form["num1"])
-            num2 = float(request.form["num2"])
-            operacion = request.form["operacion"]
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        operacion = request.form["operacion"]
 
-            if operacion == "sumar":
-                resultado = sumar(num1, num2)
-            elif operacion == "restar":
-                resultado = restar(num1, num2)
-            elif operacion == "multiplicar":
-                resultado = multiplicar(num1, num2)
-            elif operacion == "dividir":
-                resultado = dividir(num1, num2)
-            else:
-                resultado = "Operación no válida"
+        if operacion == "sumar":
+            resultado = sumar(num1, num2)
+        elif operacion == "restar":
+            resultado = restar(num1, num2)
+        elif operacion == "multiplicar":
+            resultado = multiplicar(num1, num2)
+        elif operacion == "dividir":
+            resultado = dividir(num1, num2)
+        else:
+            resultado = "Operación no válida"
+
     except ValueError:
-            resultado = "Error: Introduce números válidos"
+        resultado = "Error: Introduce números válidos"
     except ZeroDivisionError:
-            resultado = "Error: No se puede dividir por cero"
+        resultado = "Error: No se puede dividir por cero"
 
     return render_template("index.html", resultado=resultado)
 
